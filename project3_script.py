6ead187fffd9d14ad25851f1eb64d3aa6df68973
B11LA <152934960+B11LA@users.noreply.github.com>
2023-12-11 16:28:15 -0500

    Update project3_script.py

--- project3_script.py
+++ project3_script.py
@@ -6,7 +6,7 @@ import project3_module as p3m
 #%%
 fs = 500
 
-activity_1, activity_2, activity_3, activity_4 = p3m.load_data(
+sit_at_rest, relaxing_activity, mentally_stressful, physically_stressful = p3m.load_data(
     "data_example/sitting_at_rest 3.txt",
     "data_example/relaxing_activity 3.txt",
     "data_example/mentally_stressful 3.txt",
@@ -15,7 +15,7 @@ activity_1, activity_2, activity_3, activity_4 = p3m.load_data(
 )
 #%%
 concatenated_data, x_axis, activities = p3m.load_x(
-    [activity_1, activity_2, activity_3, activity_4], fs=fs, plot=False
+    [sit_at_rest, relaxing_activity, mentally_stressful, physically_stressful], fs=fs, plot=False
 )
 concatenated_time = np.arange(0, len(concatenated_data) * 1 / fs, 1 / fs)
 
@@ -24,7 +24,7 @@ num_subplots = len(activities)
 plt.figure(clear=True)
 # Create a grid of subplots based on the number of data arrays
 fig, axs = plt.subplots(num_subplots, 2, figsize=(10, 3 * num_subplots), clear=True, sharex='all')
-time = np.arange(0, len(activity_1) / fs, 1 / fs)
+time = np.arange(0, len(sit_at_rest) / fs, 1 / fs)
 filtered= p3m.filter_data(activities, 500)
 # Plot each data array on its own subplot
 for index, data_array in enumerate(activities):
@@ -49,30 +49,30 @@ plt.xlabel("Time (s)")
 plt.ylabel("Raw Data")
 plt.title("Concatenated Activities")
 plt.grid()
+
 #%%
 ''' 
 Activity 1
 '''
-ecg_analysis_A1= p3m.detect_heartbeats(activity_1, fs, plot=False)
+ecg_analysis_A1= p3m.detect_heartbeats(sit_at_rest, fs, plot=False)
 
 #%%
 '''
 Activity 2
 '''
-ecg_analysis_A2 = p3m.detect_heartbeats(activity_2, fs, plot=False)
+ecg_analysis_A2 = p3m.detect_heartbeats(relaxing_activity, fs, plot=False)
 
 #%%
 '''
 Activity 3
 '''
-ecg_analysis_A3 = p3m.detect_heartbeats(activity_3, fs, plot=False)
+ecg_analysis_A3 = p3m.detect_heartbeats(mentally_stressful, fs, plot=False)
 
 #%%
 '''
 Activity 4
 '''
-ecg_analysis_A4 = p3m.detect_heartbeats(activity_4, fs, plot=False)
-
+ecg_analysis_A4 = p3m.detect_heartbeats(physically_stressful, fs, plot=False)
 
 
 plt.show()
